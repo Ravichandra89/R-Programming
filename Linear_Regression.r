ef8b0a62e2d4df578d01ca3e8ff037f11c73fc6a
Ravi chandra <134200599+Ravichandra89@users.noreply.github.com>
2024-01-10 10:31:26 +0530

    Update Linear_Regression.r

--- Linear_Regression.r
+++ Linear_Regression.r
@@ -7,3 +7,5 @@ plot(iris$Sepal.Length ~ linear_model$fitted.values,
      ylab = "Observed Sepal.Length")
 abline(linear_model, col = "red", lty = 1)
 
+legend("topleft", legend = c("Observed Data", "Linear Regression Line"),
+       col = 1:3, lty = c(NA, 1), pch = c(1, NA))
